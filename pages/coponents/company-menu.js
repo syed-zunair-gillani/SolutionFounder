1a45a0b5a29a40f6ce504d8bdc5376ff0f006d74
zunair <gillanix007@gmail.com>
2022-05-12 01:32:28 -0700

    menu update

--- pages/coponents/company-menu.js
+++ pages/coponents/company-menu.js
@@ -1,6 +1,7 @@
 import { AiFillQuestionCircle } from 'react-icons/ai';
 import { FaUserAlt } from 'react-icons/fa';
 import { AiFillMessage } from 'react-icons/ai';
+import Link from 'next/link'
 
 export default function Company_Menu() {
   return (
@@ -8,31 +9,38 @@ export default function Company_Menu() {
       <div className="w-[200px] p-3 bg-white shadow-lg ">
         <ol className="space-y-2 ">
           <li className="text-black hover:text-[#8DC63F] text-base font-normal">
-            <a
+            <Link
               href="../inventory-warehouse-management"
               className="flex items-center"
             >
-              <span className="mr-3">
-                <AiFillQuestionCircle />
-              </span>
-              About us
-            </a>
+              <a className='flex items-center'>
+                <span className="mr-3">
+                  <AiFillQuestionCircle />
+                </span>
+                About us
+              </a>
+            </Link>
+      
           </li>
           <li className="text-black hover:text-[#8DC63F] text-base font-normal">
-            <a href="../quality-management" className="flex items-center">
-              <span className="mr-3">
-                <FaUserAlt />
-              </span>
-              Career
-            </a>
+            <Link href="../quality-management" className="flex items-center">
+              <a className='flex iitem-center'>
+                <span className="mr-3">
+                  <FaUserAlt />
+                </span>
+                Career
+              </a>
+            </Link>
           </li>
           <li className="text-black hover:text-[#8DC63F] text-base font-normal">
-            <a href="../purchase-management" className="flex items-center">
-              <span className="mr-3">
-                <AiFillMessage />
-              </span>
-              Contact Us​
-            </a>
+            <Link href="../purchase-management" className="flex items-center">
+              <a className='flex item-center'>
+                <span className="mr-3">
+                  <AiFillMessage />
+                </span>
+                Contact Us​
+              </a>
+            </Link>
           </li>
         </ol>
       </div>
