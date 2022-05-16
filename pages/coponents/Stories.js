31c6ce11a8c2a5144e82a3bd0d6eaee2d6b34121
Mufaqar Islam <mufaqar@gmail.com>
2022-05-16 15:13:57 +0500

    Stories Single with GQL

--- pages/coponents/Stories.js
+++ pages/coponents/Stories.js
@@ -7,8 +7,9 @@ export default function PostCard({ post }) {
       <div className="w-full relative flex rounded-md flex-col shadow-sh hover:shadow-shl bg-white">
         <div className="p-5 space-y-1">
           <h3 className="box-title">
-            <a href="#">{post.title}</a>
+            <a href={`stories/${post.uri}`}>{post.title}</a>
           </h3>
+
           <p className="text-base font-medium text-[#535353]">{post.excerpt}</p>
         </div>
         <div className="flex justify-end absolute right-0 bottom-0">
