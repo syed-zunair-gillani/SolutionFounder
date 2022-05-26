d2ccad9baea2787ef052f42df12bb654c4ce905c
Mufaqar Islam <mufaqar@gmail.com>
2022-05-26 20:37:35 +0500

    folder updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,15 @@
 import '../styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import { client } from '../lib/apollo';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
-
-
-
   return (
     <>
-    <Provider store={store}>
       <ApolloProvider client={client}>
-          <Component {...pageProps} />   
+        <Component {...pageProps} />
       </ApolloProvider>
-    </Provider>
-
     </>
   );
 }
 
 export default MyApp;
-
