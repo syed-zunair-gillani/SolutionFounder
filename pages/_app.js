36a9a8957a1884461f7afb13a511a2621eec6917
zunair <gillanix007@gmail.com>
2022-05-26 07:45:49 -0700

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import '../styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import { client } from '../lib/apollo';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+
 
 function MyApp({ Component, pageProps }) {
 
@@ -10,11 +9,11 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-    <Provider store={store}>
+
       <ApolloProvider client={client}>
           <Component {...pageProps} />   
       </ApolloProvider>
-    </Provider>
+
 
     </>
   );
