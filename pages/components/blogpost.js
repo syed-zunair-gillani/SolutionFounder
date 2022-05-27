d20c023b1bf415893c4e530b065f753b9da22678
Mufaqar Islam <mufaqar@gmail.com>
2022-05-27 14:50:07 +0500

    Blog

--- pages/components/blogpost.js
+++ pages/components/blogpost.js
@@ -3,16 +3,17 @@ import StoryBox from './story-box';
 export default function BlogPost({ posts }) {
   return (
     <>
-      {posts.map((data, index) => (
-        <div key={index}>
-          <StoryBox
-            icon="Awarded-as-Best-Starter-Partner-in-MENA.png"
-            imglink={data.featuredImage.node.mediaItemUrl}
-            title={data.title}
-            uri={data.uri}
-          />
-        </div>
-      ))}
+      {posts &&
+        posts.map((data, index) => (
+          <div key={index}>
+            <StoryBox
+              icon="Awarded-as-Best-Starter-Partner-in-MENA.png"
+              imglink={data.featuredImage.node.mediaItemUrl}
+              title={data.title}
+              uri={data.uri}
+            />
+          </div>
+        ))}
     </>
   );
 }
